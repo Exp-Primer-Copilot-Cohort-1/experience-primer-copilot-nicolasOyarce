f61517c355a60f4fe8e45efd593d84f6b237c827
Nicolás Oyarce <101960895+nicolasOyarce@users.noreply.github.com>
2023-11-21 18:39:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,50 @@
+// Create web server application with Express.js
+// Run the server and test it with Postman
 
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-// Create a server object
-const server = http.createServer((req, res) => {
-  // Set the response header
-  res.setHeader('Content-Type', 'text/plain');
+const app = express();
 
-  // Send a response
-  res.end('Hello, world!');
+app.use(bodyParser.json());
+
+const comments = [
+    {id: 1, content: 'Hello'},
+    {id: 2, content: 'World'}
+];
+
+app.get('/api/comments', (req, res) => {
+    res.send(comments);
 });
 
-// Start the server
-server.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.post('/api/comments', (req, res) => {
+    const comment = {
+        id: comments.length + 1,
+        content: req.body.content
+    };
+    comments.push(comment);
+    res.send(comment);
 });
+
+app.put('/api/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) {
+        res.status(404).send('The comment with the given ID was not found.');
+        return;
+    }
+    comment.content = req.body.content;
+    res.send(comment);
+});
+
+app.delete('/api/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) {
+        res.status(404).send('The comment with the given ID was not found.');
+        return;
+    }
+    const index = comments.indexOf(comment);
+    comments.splice(index, 1);
+    res.send(comment);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
